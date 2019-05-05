a740d96c4235b46037f2428f8bb2ef1598e76b1d
MatteoManzoni <manzoni.matteo@mailfence.com>
2019-05-05 14:45:45 +0200

    Fix start page search

--- .webrc/scripts.js
+++ .webrc/scripts.js
@@ -1,8 +1,16 @@
 // Search on enter key event
+function search_visibility(e) {
+    if (e.keyCode == 27 && (document.getElementById("search").style.display != "" || document.getElementById("search").style.display != "none")) {
+        document.getElementById("search").style.display = "";   
+    }
+    if (document.getElementById("search").style.display != "flex") {
+        document.getElementById("search").style.display = "flex";
+        document.getElementById("search-field").select();
+    }
+
+}
 function search(e) {
-    alert("keycode: "+e.keyCode);
-    if (e.keyCode == 18) {
-        
+    if (e.keyCode == 13) {
         var val = document.getElementById("search-field").value;
         window.open("https://google.com/search?q=" + val);
     }
